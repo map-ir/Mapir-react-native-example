e1bacde370a8ba92df3dcb1cbda965954b4d7725
Reyhane Masumi <r.masumi94@gmail.com>
2019-01-22 13:59:39 +0330

    init

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import Mapir from 'mapir-react-native-sdk'
+
+type Props = {};
+export default class App extends Component<{}> {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Mapir
+                    accessToken={'YOUR MAPIR TOKEN'}
+                    zoomLevel={13}
+                    centerCoordinate={[51.422548, 35.732573]}
+                    style={styles.container}>
+                </Mapir>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+});

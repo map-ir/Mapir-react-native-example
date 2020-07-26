6951cc3cdca8f6966ad6e5eae36a4d428d4febbc
Reyhane Masumi <R.Masumi94@gmail.com>
2020-07-26 13:52:37 +0430

    ver 0.62.2

--- App.js
+++ App.js
@@ -3,211 +3,34 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow
+ * @flow strict-local
  */
 
-import  React, {Component} from 'react';
-import {Platform, StyleSheet, TextInput, View, Button, PermissionsAndroid} from 'react-native';
-import axios from 'axios';
-import polyline from 'google-polyline';
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
 import Mapir from 'mapir-react-native-sdk';
 
-type Props = {};
-export default class App extends Component<Props> {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: '',
-            route: {},
-            routeLine: [],
-            Markers: [],
-            Map: [],
-            location: [51.399863, 35.789938]
-        };
-        this.MapView = this.MapView.bind(this)
-    }
-
-    componentDidMount() {
-        if(Platform === 'android')
-        {
-            PermissionsAndroid.requestMultiple(
-                [PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                    PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION],
-                {
-                    title: 'Give Location Permission',
-                    message: 'App needs location permission to find your position.'
-                }
-            ).then(granted => {
-                console.log(granted);
-                resolve();
-            }).catch(err => {
-                console.warn(err);
-                reject(err);
-            });
-        }
-    }
-
-    search(text) {
-        var _this = this;
-        var dataMarker = [];
-        axios({
-            method: 'post',
-            url: 'https://map.ir/search',
-            headers: {
-                "x-api-key": "Mapir.WsLdHK46I5Wfr5xgI0ynjjyiw9Fyhydu"
-            },
-            data: {
-                "$top": 20,
-                "text": text,
-                "$select": "poi",
-                "$filter": "province eq تهران",
-                "location": {
-                    "type": "Point",
-                    "coordinates": [51.399863, 35.789938]
-                }
-            }
-        }).then(function (data) {
-            _this.setState({
-                Markers: []
-            });
-            data.data.value.forEach(function (item, index) {
-                dataMarker.push(
-                    <Mapir.Marker key={index}
-                                  id={'1'}
-                                  title={item.Title}
-                                  snippet={item.Title}
-                                  coordinate={[item.Coordinate.lon, item.Coordinate.lat]}
-                                  onSelected={() => _this.route(item.Coordinate.lat, item.Coordinate.lon)}>
-                        <Mapir.Popup title={item.Title}/>
-                    </Mapir.Marker>
-                );
-            });
-            _this.setState({
-                Markers: dataMarker
-            })
-        }).catch(function (error) {
-            console.log('errorrrrr', error);
-        })
-    }
-
-    route(lat, lon) {
-        var _this = this;
-        var dataRoute = [];
-        _this.setState({
-            route: [],
-            routeLine: [],
-            location: [lon, lat]
-        });
-        axios({
-            method: 'get',
-            url: `https://map.ir/routes/route/v1/driving/51.399863,35.789938;${lon},${lat}?alternatives=true&steps=true&overview=full`,
-            headers: {
-                "x-api-key": "Mapir.WsLdHK46I5Wfr5xgI0ynjjyiw9Fyhydu"
-            },
-        }).then(function (data) {
-            var coordinates = [];
-            polyline.decode(data.data.routes[0].geometry).forEach(function (item, index) {
-                coordinates[index] = item.reverse()
-            })
-            _this.setState({
-                route: {
-                    "type": "FeatureCollection",
-                    "features": [
-                        {
-                            "type": "Feature",
-                            "properties": {},
-                            "geometry": {
-                                "type": "LineString",
-                                "coordinates": coordinates
-                            }
-                        }
-                    ]
-                }
-            });
-            dataRoute.push(
-                <Mapir.ShapeSource key={1} id='line1' shape={_this.state.route}>
-                    <Mapir.LineLayer id='linelayer1' style={{lineColor: 'red'}}/>
-                </Mapir.ShapeSource>
-            )
-            _this.setState({
-                routeLine: dataRoute
-            })
-        }).catch(function (err) {
-            console.log(err);
-        })
-    }
-
-    MapView() {
-        var _this = this
-        var map = []
-        _this.setState({
-            Map: []
-        })
-        map.push(
-            <Mapir
-                key={1}
-                accessToken={'Mapir.WsLdHK46I5Wfr5xgI0ynjjyiw9Fyhydu'}
-                zoomLevel={13}
-                centerCoordinate={[51.399863, 35.789938]}
-                showUserLocation={true}
-                style={styles.map} />
-        )
-        _this.setState({
-            Map: map
-        })
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <TextInput placeholder='search'
-                           onChangeText={(text) => this.search(text)}
-                           style={styles.input}/>
-                <Mapir
-                    accessToken={'Mapir.WsLdHK46I5Wfr5xgI0ynjjyiw9Fyhydu'}
-                    zoomLevel={13}
-                    centerCoordinate={[51.399863, 35.789938]}
-                    attributionEnabled={true}
-                    style={styles.map}>
-                    <Mapir.Marker id={'1'}
-                                  title={'ما اینجا هستیم'}
-                                  coordinate={[51.399863, 35.789938]}>
-                        <Mapir.Popup title={'ما اینجا هستیم'}/>
-                    </Mapir.Marker>
-                    <Mapir.UserLocation animated={true}/>
-                    <Mapir.Camera
-                        zoomLevel={16}
-                        animationMode={'flyTo'}
-                        animationDuration={6000}
-                        centerCoordinate={this.state.location}
-                    />
-                    {this.state.Markers}
-                    {this.state.routeLine}
-                </Mapir>
-                {/*<Button title={"Map"} onPress={this.MapView} style={styles.button}/>*/}
-                {/*<View>*/}
-                    {/*{this.state.Map}*/}
-                {/*</View>*/}
-            </View>
-        );
-    }
-}
+const App: () => React$Node = () => {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <Mapir
+          apiKey={
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImZkNDRkNWQ0NjgwY2U0ZTM2NDlmNGEwNjBiZDkzZjk1Yzk4MmE3OTdjMzVmZDFiNzNiZGY4ZjhkNDg2YmY1NzdmNmU1NjI5NDkwYjlkNzE4In0.eyJhdWQiOiI3ODA0IiwianRpIjoiZmQ0NGQ1ZDQ2ODBjZTRlMzY0OWY0YTA2MGJkOTNmOTVjOTgyYTc5N2MzNWZkMWI3M2JkZjhmOGQ0ODZiZjU3N2Y2ZTU2Mjk0OTBiOWQ3MTgiLCJpYXQiOjE1ODA3NDEwNDAsIm5iZiI6MTU4MDc0MTA0MCwiZXhwIjoxNTgzMjQ2NjQwLCJzdWIiOiIiLCJzY29wZXMiOlsiYmFzaWMiXX0.aEvCWqPzYtyErla-8_KcipBX0dwZge1mq796BkjWs-siXny3aOkL57NMabScYg3YRP_dc72eN1HxfnPewOT-_c8Qlgk0OcAm5joWFw8XYZPo4rl9zOlATdReZU-Luu-O7IcGx_CJWR48Jh4ZbBQz0IEaCBxNdGFuaThDrJbB4Gkf4OdgJJIa-OLYbC1psiFLS7B6L0bu5GBSfphPIXzxAgUWrwtqWMAJ-sh1WPhpcLIgtU6p97FzPay1VmRxrQTsA8sNEYah2bupxUqfgK1mXlpQ-dz_bEG-Zq9b9ZK_G5c5On-lqkEUr7pyBHsEIr5xSzeBmAMmyvItKBpzLKOPZw'
+          }
+          zoomLevel={13}
+          centerCoordinate={[51.422548, 35.732573]}
+          style={styles.container}></Mapir>
+      </SafeAreaView>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 2
-    },
-    map: {
-        flex: 1,
-        height: '50%',
-        width: '100%'
-    },
-    input: {
-        height: '10%'
-    },
-    button: {
-        width: '100%',
-        height: '10%',
-        backgroundColor: 'red'
-    }
+  container: {
+    flex: 1,
+  },
 });
+
+export default App;
